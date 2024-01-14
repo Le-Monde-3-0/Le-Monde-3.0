48a9670d66aba20c6f4f21a890ba8045a378c4bd
Adrien FORT <adrien.fort01@gmail.com>
2024-01-14 03:58:10 +0100

    add: ipfs script running its IPFS node using docker

--- frontend/web/ipfs/publish.sh
+++ frontend/web/ipfs/publish.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-SCALEWAY_GET_PINS_OUTPUT="scaleway_pins.txt"
+IPFS_HOST="ipfs_host"
+SCALEWAY_GET_PINS_OUTPUT="scaleway_get_pins.txt"
+SCALEWAY_POST_PIN_OUTPUT="scaleway_post_pin.txt"
+SCALEWAY_GET_PIN_OUTPUT="scaleway_get_pin.txt"
 SCALEWAY_REGION_ID="fr-par"
 SCALEWAY_VOLUME_ID="80c8d0e5-539a-4bed-af4a-77c0d767684a"
 SCALEWAY_NAME_ID="2e839d89-8294-4eab-87c1-f2156a67cac4"
@@ -12,22 +15,44 @@ exit_if_failure() {
     exit_status=$?
     if [ $exit_status != 0 ]; then
         echo $1
+        docker stop $IPFS_HOST
+        docker remove $IPFS_HOST
         exit $exit_status
     fi
 }
 
+# 0. Setup
+echo "[Step 0]. Pull IPFS docker image and set env variables"
+cd ../
+WORKDIR=$(pwd)
+cd ./ipfs
+export ipfs_staging=$WORKDIR
+export ipfs_data="$WORKDIR/ipfs"
+docker pull ipfs/kubo
+exit_if_failure "Failed to pull IPFS docker image."
+echo ""
+
 # 1. Build frontend
-echo "1. Building frontend..."
-cd ../ && npm run build && cd ipfs
+echo "[Step 1]. Build frontend"
+cd ../ && npm install && npm run build && cd ipfs
 exit_if_failure "Failed to build frontend."
 
-# 2. Add frontend build to IPFS and get CID
-echo "2. Adding frontend build to IPFS..."
-NEW_CID=$(ipfs add -Q -r ../build)
-exit_if_failure "Failed to add to IPFS."
+# 2. Run IPFS daemon
+echo "[Step 2]. Run IPFS Daemon"
+docker run -d --name $IPFS_HOST -v $ipfs_staging:/export -v $ipfs_data:/data/ipfs -p 4001:4001 -p 4001:4001/udp -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001 ipfs/kubo:latest
+exit_if_failure "Failed to run IPFS docker image."
+sleep 3
+echo ""
 
-# 3. Update IPFS name with new CID
-echo "3. Updating Scaleway name $SCALEWAY_NAME_ID with new CID..."
+# 3. Add frontend build to IPFS and get CID
+echo "[Step 3]. Add frontend build to IPFS"
+NEW_CID=$(docker exec $IPFS_HOST ipfs add -Q -r /export/build)
+exit_if_failure "Failed to add to IPFS."
+echo "frontend build new CID is $NEW_CID"
+echo ""
+
+# 4. Update IPFS name with new CID
+echo "[Step 4]. Update Scaleway name $SCALEWAY_NAME_ID with new CID"
 curl -f -X PATCH \
     -H "X-Auth-Token: $SCALEWAY_AUTH_TOKEN" \
     -H "Content-Type: application/json" \
@@ -35,35 +60,38 @@ curl -f -X PATCH \
         "value": "'"$NEW_CID"'"
     }' \
     "https://api.scaleway.com/ipfs/v1alpha1/regions/$SCALEWAY_REGION_ID/names/$SCALEWAY_NAME_ID"
-exit_if_failure "Failure."
+exit_if_failure "Failed to update name with new CID."
+echo ""
 echo ""
 
-# 4. Get Scaleway pins
-echo "4. Getting Scalway pins..."
+# 5. Get Scaleway pins
+echo "[Step 5]. Get Scalway pins"
 curl -f -X GET \
     -H "Content-Type: application/json" \
     -H "X-Auth-Token: $SCALEWAY_AUTH_TOKEN" \
     "https://api.scaleway.com/ipfs/v1alpha1/regions/$SCALEWAY_REGION_ID/pins?order_by=created_at_asc&volume_id=$SCALEWAY_VOLUME_ID" \
     >$SCALEWAY_GET_PINS_OUTPUT
+exit_if_failure "Failed to get Scaleway pins."
 cat $SCALEWAY_GET_PINS_OUTPUT
-exit_if_failure "Failure."
+echo ""
 echo ""
 
-# 5. Delete Scaleway pins
-echo "5. Deleting Scaleway pins..."
+# 6. Delete Scaleway pins
+echo "[Step 6]. Delete Scaleway pins"
 PINS=$(jq -r '.pins[] | .pin_id' $SCALEWAY_GET_PINS_OUTPUT)
 for pin in $PINS; do
     curl -f -X DELETE \
         -H "Content-Type: application/json" \
         -H "X-Auth-Token: $SCALEWAY_AUTH_TOKEN" \
         "https://api.scaleway.com/ipfs/v1alpha1/regions/$SCALEWAY_REGION_ID/pins/$pin?volumes/$SCALEWAY_VOLUME_ID"
-    exit_if_failure "Failure."
+    exit_if_failure "Failed to delete Scaleway pin $pin."
     echo "- pin $pin deleted."
 done
 rm -f $SCALEWAY_GET_PINS_OUTPUT
+echo ""
 
-# 6. Pin CID
-echo "6. Pinning new CID..."
+# 7. Pin CID
+echo "[Step 7]. Pin new CID using Scaleway"
 curl -f -X POST \
     -H "X-Auth-Token: $SCALEWAY_AUTH_TOKEN" \
     -H "Content-Type: application/json" \
@@ -72,12 +100,48 @@ curl -f -X POST \
         "name": "'"$SCALEWAY_PIN_NAME"'",
         "volume_id": "'"$SCALEWAY_VOLUME_ID"'"
     }' \
-    "https://api.scaleway.com/ipfs/v1alpha1/regions/$SCALEWAY_REGION_ID/pins/create-by-cid"
-exit_if_failure "Failure."
-
+    "https://api.scaleway.com/ipfs/v1alpha1/regions/$SCALEWAY_REGION_ID/pins/create-by-cid" \
+    >$SCALEWAY_POST_PIN_OUTPUT
+exit_if_failure "Failed to pin new CID."
+cat $SCALEWAY_POST_PIN_OUTPUT
 echo ""
+echo ""
+
+# 8. Wait for pin
+echo "[Step 8]. Wait for new PIN to have status 'pinned'"
+NEW_PIN_ID=$(jq -r '.pin_id' $SCALEWAY_POST_PIN_OUTPUT)
+PINNED=false
+while [ "$PINNED" = "false" ]; do
+    sleep 1
+    curl -f -X GET \
+        -H "X-Auth-Token: $SCALEWAY_AUTH_TOKEN" \
+        "https://api.scaleway.com/ipfs/v1alpha1/regions/$SCALEWAY_REGION_ID/pins/$NEW_PIN_ID?volume_id=$SCALEWAY_VOLUME_ID" \
+        >$SCALEWAY_GET_PIN_OUTPUT
+    exit_if_failure "Failed to get new pin $NEW_PIN_ID information."
+    STATUS=$(jq -r '.status' $SCALEWAY_GET_PIN_OUTPUT)
+    echo ". pin $NEW_PIN_ID status is $STATUS"
+    if [ "$STATUS" = "pinned" ]; then
+        PINNED=true
+    fi
+done
+rm -f $SCALEWAY_GET_PIN_OUTPUT
+rm -f $SCALEWAY_POST_PIN_OUTPUT
+echo ""
+
+# 9. Cleaning
+echo "[Step 9]. Stop IPFS docker image and remove it"
+docker stop $IPFS_HOST
+docker remove $IPFS_HOST
+echo ""
+
+# 10. Conclusion
 echo "Congratulations! The frontend build has been successfully:"
 echo "- added to IPFS, its CID is $NEW_CID"
 echo "- pinned"
 echo "- linked to the name '$SCALEWAY_PIN_NAME'"
-echo "Previous pins have also been deleted."
+echo "Note: previous pins have also been deleted."
+echo ""
+echo "You can run:"
+echo "- ipfs name resolve $SCALEWAY_NAME_VALUE"
+echo "- ipfs get $NEW_CID"
+echo "Note: don't forget to run your IPFS node!"
