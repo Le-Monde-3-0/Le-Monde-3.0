56fc339358aeb27d412b81e425b78f4fc767184e
Fares <fares.haouari@epitech.eu>
2024-01-15 21:55:28 -0800

    [+] notif function

--- backend/articles/sources/post.go
+++ backend/articles/sources/post.go
@@ -8,6 +8,32 @@ import (
 	"strconv"
 )
 
+
+type Notification struct {
+	gorm.Model
+	UserID     int32
+	AuthorID   int32
+	ArticleID  int32
+	IsRead     bool
+}
+
+func createNotification(db *gorm.DB, userID, authorID, articleID int32) error {
+	notification := Notification{
+		UserID:    userID,
+		AuthorID:  authorID,
+		ArticleID: articleID,
+		IsRead:    false,
+	}
+
+	result := db.Create(&notification)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+
 func AddArticle(c *gin.Context, db *gorm.DB) {
 	article := new(Article)
 
@@ -33,6 +59,16 @@ func AddArticle(c *gin.Context, db *gorm.DB) {
 	if result.Error != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": result.Error})
 		return
+	}
+
+	// Créez des notifications pour les utilisateurs qui suivent cet auteur
+	var followers []Follower
+	db.Where(Follower{AuthorID: article.UserId}).Find(&followers)
+	for _, follower := range followers {
+		err := createNotification(db, follower.UserID, article.UserId, article.ID)
+		if err != nil {
+			return 
+		}
 	}
 	c.JSON(http.StatusCreated, article)
 }
