e10dbe93639e64d6bef09fa5ea2f672b3881973a
Adrien FORT <adrien.fort01@gmail.com>
2024-01-17 05:14:40 +0100

    fix: type error

--- frontend/web/src/services/articles/read.ts
+++ frontend/web/src/services/articles/read.ts
@@ -1,7 +1,7 @@
 import { servicesURL } from 'services';
 import { Article } from 'types/article';
 
-const read = async ({ token, articleId }: { token: string; articleId: string }) =>
+const read = async ({ token, articleId }: { token: string; articleId: number }) =>
 	servicesURL.get<Article>(`/articles/${articleId}`, {
 		headers: { Authorization: `Bearer ${token}` },
 	});
