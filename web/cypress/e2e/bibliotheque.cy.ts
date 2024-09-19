ea0d1c40fc695431defe06b3f39fb1df19442d85
Adrien FORT <adrien.fort01@gmail.com>
2024-09-19 15:46:27 +0200

    fix: remove useless tests

--- web/cypress/e2e/bibliotheque.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('[LAYOUT] Bibliotheque page', () => {
-	beforeEach('Go to bibliotheque page', () => {
-		cy.visit('/bibliotheque');
-	});
-
-	it('Redirection', () => {
-		cy.get('#app-title').should('contain', 'Anthologia');
-	});
-});
